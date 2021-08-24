c4de9d86f7601858d0e4d184383a490e0597bc36
Diego Rodriguez <diego.gabrielrodriguez@hotmail.com>
2021-08-24 02:17:20 -0300

    fixing routes

--- src/App.js
+++ src/App.js
@@ -19,12 +19,12 @@ function App() {
     <UserProvider>
       <Router>
         <Switch>
-          <Route exact path="/twitter-clone/login" component={Login} />
+          <Route exact path="/login" component={Login} />
           <div className="home">
             <TweetProvider>
 
               <Aside />
-              <Route exact path="/twitter-clone/" component={TweetsContainer}/>
+              <Route exact path="/" component={TweetsContainer}/>
               <Route exact path="/tweet/:id"  component={TweetDetailContainer} />
 
             </TweetProvider>
