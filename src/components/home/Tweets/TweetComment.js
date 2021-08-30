32666f3297ba89d009f2f92c5b2fe09e3e2b4123
Diego Rodriguez <diego.gabrielrodriguez@hotmail.com>
2021-08-30 18:09:30 -0300

    fixing responsive

--- src/components/home/Tweets/TweetComment.js
+++ src/components/home/Tweets/TweetComment.js
@@ -5,17 +5,19 @@ import { TweetContext } from "../../../context/TweetContext";
 const TweetComment = ({ showModal, setShowModal, children, tweetComment }) => {
   const { handleSendComment } = useContext(TweetContext);
   return (
-    <div className="tweet-modal">
-      <div className="tweet-modal-close">
-        <MdClose onClick={() => setShowModal(!showModal)} />
+    <div className="tweet-modal-container">
+      <div className="tweet-modal">
+        <div className="tweet-modal-close">
+          <MdClose onClick={() => setShowModal(!showModal)} />
+        </div>
+        {children}
+        <TweetBox
+          placeholder="Twittea tu respuesta"
+          button="Responder"
+          handleSendTweet={handleSendComment}
+          tweetComment={tweetComment}
+        />
       </div>
-      {children}
-      <TweetBox
-        placeholder="Twittea tu respuesta"
-        button="Responder"
-        handleSendTweet={handleSendComment}
-        tweetComment={tweetComment}
-      />
     </div>
   );
 };
